320d9ccd130e4cc9431ae27a1bf53ab70affb276
smorton <smorton@800is700-l.us.chs.net>
2019-09-12 08:58:47 -0500

    dded an xfreerdp script to allow uniform execution form the command line. Copy into your /usr/bin folder and make sure it is executable.

---
+++ rdp.sh
@@ -0,0 +1,34 @@
+
+echo
+echo "Usage: ./rdp.sh servername username@somedomain.dom"
+echo
+
+if [ $# -eq 0 ]
+then
+    echo -n "Server: "
+    read  server
+    echo
+    echo -n "UID: "
+    read  uid
+    echo
+fi
+
+if [ $# -eq 1 ] || [ $# -gt 2 ]
+then
+    exit 1
+fi
+
+echo
+echo -n Password:
+read -s password
+echo
+
+if [ $# -eq 2 ]
+then
+    server=$1
+    uid=$2
+fi
+
+xfreerdp /w:1700 /h:920 /kbd:0x00000409 /bpp:24 /audio-mode:2 /sound:sys:alsa /drive:media,/home/$USER /network:lan /cert-tofu +clipboard /port:3389 /v:$server /u:$uid /p:$password
+
+#xfreerdp /cert-tofu /bpp:24 /w:1600 /h:900 /t:$server /drive:/home/smorton /v:$server /u:$uid /p:$password
